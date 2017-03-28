4982b138d5b8cc48381430057a4b22cf04a3926e
haoxin <haoxins@outlook.com>
2017-03-28 17:14:29 +0800

    add dev server

---
+++ server.js
@@ -0,0 +1,25 @@
+
+const config = require('./webpack.dev')
+const express = require('express')
+const webpack = require('webpack')
+
+const compiler = webpack(config)
+const app = express()
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  publicPath: config.output.publicPath,
+  noInfo: true,
+  quiet: true
+}))
+
+app.use(require('webpack-hot-middleware')(compiler))
+
+app.use(express.static(__dirname))
+
+app.listen(3000, err => {
+  if (err) {
+    return console.error(err)
+  }
+
+  console.info('Listening at http://localhost:3000')
+})
