14860aac8a237ed45d19e61a0a1fafcfb63f312c
haoxin <haoxins@outlook.com>
2017-03-31 16:13:04 +0800

    sidebar init

---
+++ component/bax.vue
@@ -0,0 +1,41 @@
+
+<template>
+  <content>
+    <sidebar></sidebar>
+    <main>{{ msg }}</main>
+  </content>
+</template>
+
+<script>
+
+import Sidebar from './sidebar'
+
+export default {
+  name: 'bax',
+  components: {
+    Sidebar
+  },
+  data() {
+    return {
+      msg: 'bingo'
+    }
+  }
+}
+
+</script>
+
+<style>
+
+@import 'cssbase/reset';
+
+</style>
+
+<style scoped>
+
+content {
+  display: flex;
+  width: 100%;
+  height: 100%;
+}
+
+</style>
---
+++ component/sidebar/index.vue
@@ -0,0 +1,56 @@
+
+<template>
+  <main>
+    <header>
+      BAX SSP
+    </header>
+    <content>
+      <el-menu default-active="1">
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-message"></i>投放管理
+          </template>
+          <el-menu-item index="1-1">广告投放</el-menu-item>
+          <el-menu-item index="1-2">物料管理</el-menu-item>
+          <el-menu-item index="1-3">订单管理</el-menu-item>
+          <el-menu-item index="1-4">客户管理</el-menu-item>
+        </el-submenu>
+        <el-menu-item index="1-1">
+          <i class="el-icon-menu"></i>设置
+        </el-menu-item>
+        <el-menu-item index="1-1">
+          <i class="el-icon-setting"></i>账户
+        </el-menu-item>
+      </el-menu>
+    </content>
+  </main>
+</template>
+
+<script>
+
+export default {
+  name: 'sidebar'
+}
+
+</script>
+
+<style scoped>
+
+@import '../../cssbase/var';
+
+main {
+  & header {
+    display: flex;
+    justify-content: center;
+    padding-top: 15px;
+    height: 90px;
+    font-size: 20px;
+    color: #768396;
+  }
+
+  width: 180px;
+  height: 100%;
+  background: var(--c-gray);
+}
+
+</style>
