d1beef2dabc613b0c1f90f3f0c6feefda169bd3a
haoxin <haoxins@outlook.com>
2017-04-10 17:10:40 +0800

    component - add Topbar

---
+++ component/topbar/index.vue
@@ -0,0 +1,47 @@
+
+<template>
+  <header>
+    <span>
+      <i class="el-icon-arrow-left"></i>
+      <slot name="title"></slot>
+    </span>
+    <span>
+      <i class="el-icon-message"></i>
+      <el-dropdown>
+        <span class="el-dropdown-link">
+          {{ userInfo.name }}
+          <i class="el-icon-caret-bottom el-icon--right"></i>
+        </span>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>修改密码</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+    </span>
+  </header>
+</template>
+
+<script>
+
+export default {
+  name: 'topbar',
+  props: {
+    userInfo: {
+      type: Object,
+      required: true
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  min-height: 60px;
+  height: 60px;
+}
+
+</style>
