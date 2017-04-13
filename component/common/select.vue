21cfef4531515bc4ba989cce2f8a1c4ff1dd3bab
haoxin <haoxins@outlook.com>
2017-04-13 23:16:24 +0800

    add common component - Select

---
+++ component/common/select.vue
@@ -0,0 +1,42 @@
+
+<template>
+  <el-select v-model="value">
+    <el-option v-for="opt in options"
+      :label="opt.label"
+      :value="opt.value">
+    </el-option>
+  </el-select>
+</template>
+
+<script>
+
+export default {
+  name: 'bax-select',
+  props: {
+    options: {
+      type: Array,
+      required: true
+    },
+    onChange: {
+      type: Function,
+      required: true
+    },
+    multiple: Boolean
+  },
+  data() {
+    return {
+      value: ''
+    }
+  },
+  watch: {
+    value: function(v) {
+      this.onChange(v)
+    }
+  }
+}
+
+</script>
+
+<style>
+
+</style>
