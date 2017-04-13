2ef5d12b9e3b4f88c9dad141aff8f7f7aaac6795
haoxin <haoxins@outlook.com>
2017-04-13 22:24:00 +0800

    constant++

---
+++ constant/ad.js
@@ -0,0 +1,20 @@
+
+export const platform = {
+  '0': 'PC',
+  '1': 'WAP',
+  '2': 'APP'
+}
+
+export const adType = {
+  '0': '文字',
+  '1': '图片',
+  '2': '图文'
+}
+
+export const adStatus = {
+  '-10': '审核失败',
+  '-1': '下线',
+  '0': '等待审核',
+  '1': '准备投放',
+  '2': '投放中'
+}
---
+++ constant/coupon.js
@@ -0,0 +1,6 @@
+
+export const couponStatus = {
+  '-1': '无效',
+  '0': '未使用',
+  '1': '已使用'
+}
