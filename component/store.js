50720d9ffb602040ab0c80ad97e007a05cc8d80b
haoxin <haoxins@outlook.com>
2017-04-13 23:09:23 +0800

    main store - add meta data

--- component/store.js
+++ component/store.js
@@ -2,16 +2,27 @@
 import { createStore } from 'vue-duo'
 
 import {
-  getCurrentUser
+  getCurrentUser,
+  getCategories,
+  getAreas
 } from './action'
 
 const store = createStore({
+  allCategories: [],
+  allAreas: [],
+
   currentUser: {}
 })
 
 store.subscribeActions({
   [getCurrentUser]: (user) => ({
     currentUser: {...user}
+  }),
+  [getCategories]: (categories) => ({
+    allCategories: [...categories]
+  }),
+  [getAreas]: (areas) => ({
+    allAreas: [...areas]
   })
 })
 
