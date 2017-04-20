8b784f750475190d7bbb0670058665bd38c698f9
haoxin <haoxins@outlook.com>
2017-04-20 12:13:30 +0800

    订单查价 * 新建订单

--- api/ad.js
+++ api/ad.js
@@ -8,7 +8,7 @@ export async function getAdPrice(aid, opts) {
     .query(reverseCamelcase(opts))
     .json()
 
-  return toCamelcase(body.data)
+  return body.data
 }
 
 export async function getAds() {
--- component/order-create/index.vue
+++ component/order-create/index.vue
@@ -7,7 +7,8 @@
     <main>
       <el-form ref="form" :model="newOrder" label-width="120px">
         <el-form-item label="广告位">
-          <el-input v-model="newOrder.adId"></el-input>
+          <bax-select :options="adOpts"
+            @change="v => newOrder.adId = v" />
         </el-form-item>
         <el-form-item label="城市">
           <bax-select :options="allAreas" multiple
@@ -31,6 +32,10 @@
         <el-form-item label="广告客户">
           <el-input v-model="newOrder.userId"></el-input>
         </el-form-item>
+        <section class="ad-price" v-if="adPrice">
+          <label>价格:</label>
+          <span>{{ '￥' + adPrice }}</span>
+        </section>
         <el-form-item>
           <el-button>
             <router-link tag="p" :to="{ name: 'order-list' }">
@@ -49,16 +54,35 @@
 <script>
 
 import BaxSelect from 'com/common/select'
+import { Message } from 'element-ui'
 import Topbar from 'com/topbar'
 
-import { toTimestamp } from 'utils'
+import { getAdPrice } from 'api/ad'
+import {
+  toTimestamp,
+  centToYuan
+} from 'utils'
+
+import store from './store'
 
 import {
-  createOrder
+  createOrder,
+  getAds
 } from './action'
+
+const emptyOrder = {
+  adId: 0,
+  categories: [],
+  cities: [],
+  onlineAt: Date.now(),
+  offlineAt: Date.now(),
+  salesId: 0,
+  userId: 0
+}
 
 export default {
   name: 'create-order',
+  store,
   props: {
     allCategories: {
       type: Array,
@@ -79,14 +103,58 @@ export default {
   },
   data() {
     return {
-      newOrder: {
-      }
+      newOrder: {...emptyOrder},
+      adPrice: ''
+    }
+  },
+  computed: {
+    adOpts() {
+      return this.ads.map(ad => ({
+        label: ad.id + ' ' + ad.slotCode,
+        value: ad.id
+      }))
     }
   },
   async mounted() {
-
+    await getAds()
+  },
+  watch: {
+    newOrder: {
+      handler(newOrder) {
+        this.queryAdPrice(newOrder)
+      },
+      deep: true
+    }
   },
   methods: {
+    empty() {
+      this.newOrder = {...emptyOrder}
+      this.adPrice = ''
+    },
+    async queryAdPrice(newOrder) {
+      const {
+        categories,
+        cities,
+
+        offlineAt,
+        onlineAt,
+        adId
+      } = newOrder
+
+      const opts = {
+        startAt: toTimestamp(onlineAt),
+        endAt: toTimestamp(offlineAt),
+        categories: [...categories],
+        cities: [...cities],
+        adId
+      }
+
+      if (opts.adId && opts.categories.length && opts.cities.length
+        && opts.startAt && opts.endAt) {
+          const price = await getAdPrice(opts.adId, opts)
+          this.adPrice = centToYuan(price)
+        }
+    },
     async onSubmit() {
       const { newOrder } = this
 
@@ -97,6 +165,14 @@ export default {
       }
 
       await createOrder(data)
+
+      this.empty()
+
+      Message.success('创建成功')
+
+      this.$router.push({
+        name: 'order-list'
+      })
     }
   }
 }
@@ -110,6 +186,21 @@ export default {
 
   & > main {
     max-width: 680px;
+  }
+
+  & .ad-price {
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+
+    & > label {
+      color: #5e6d82;
+    }
+
+    & > span {
+      font-size: 22px;
+      color: red;
+    }
   }
 }
 
