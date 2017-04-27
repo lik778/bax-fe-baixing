448ac9ee5ed9514e4aa328fae4435baefadfae14
haoxin <haoxins@outlook.com>
2017-04-27 21:53:42 +0800

    物料详情

--- component/material-info/index.vue
+++ component/material-info/index.vue
@@ -6,7 +6,14 @@
     </topbar>
     <main>
       <section>
-        <img v-bind:src="material.imgUrl" />
+        <img class="img" v-bind:src="material.imgUrl" />
+      </section>
+      <section>
+        <item label="名称" :value="material.name" />
+        <item label="类型" :value="getMType(material.materialType)" />
+        <item label="规格" :value="material.slot" />
+        <item label="内容" :value="material.content" />
+        <item tag="a" label="链接" :value="material.link" />
       </section>
     </main>
   </div>
@@ -15,8 +22,13 @@
 <script>
 
 import Topbar from 'com/topbar'
+import Item from './item'
 
 import store from './store'
+
+import {
+  materialType
+} from 'constant/material'
 
 import {
   getMaterial
@@ -32,7 +44,13 @@ export default {
     }
   },
   components: {
-    Topbar
+    Topbar,
+    Item
+  },
+  methods: {
+    getMType(s) {
+      return materialType[String(s)]
+    }
   },
   async mounted() {
     const id = this.$route.params.id
@@ -43,6 +61,12 @@ export default {
 </script>
 
 <style scoped>
+
+.img {
+  max-width: 400px;
+  max-height: 400px;
+  object-fit: contain;
+}
 
 .material-info {
   padding: 0 35px;
---
+++ component/material-info/item.vue
@@ -0,0 +1,53 @@
+
+<template>
+  <div>
+    <span>{{ label }}</span>
+    <a v-if="tag === 'a'" :href="value" target="_blank">
+      {{ value }}
+    </a>
+    <span v-else>
+      {{ value }}
+    </span>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'material-info-item',
+  props: {
+    tag: String,
+    label: {
+      type: String,
+      required: true
+    },
+    value: {
+      type: [String, Number]
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+div {
+  display: flex;
+  align-items: center;
+  margin: 10px 0;
+
+  & > span:first-child {
+    width: 80px;
+    margin-right: 10px;
+    font-size: 14px;
+    color: #5e6d82;
+    text-align: right;
+  }
+
+  & > span:last-child, & > a:last-child {
+    font-size: 14px;
+    color: #1f2d3d;
+  }
+}
+
+</style>
