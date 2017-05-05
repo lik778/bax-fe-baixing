fab930ee7880e5acc0623908e7df415cc8b29f14
haoxin <haoxins@outlook.com>
2017-05-05 14:48:27 +0800

    config ++

--- config/index.js
+++ config/index.js
@@ -1,6 +1,10 @@
 
+const isPro = location.hostname.includes('bax.baixing.com.cn')
+
 export const qiniuHost = '//ono2r7ueh.bkt.clouddn.com/'
 
-export const baxApiHost = 'http://192.168.8.201/bax'
+export const baxApiHost = isPro
+  ? '//bax.baixing.com.cn/api'
+  : '//192.168.8.201/bax'
 
 export const assetHost = ''
