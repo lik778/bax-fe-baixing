ebed1bd2da55deae1b076a9279784578d356ea39
haoxin <haoxins@outlook.com>
2017-05-17 15:27:12 +0800

    util++

--- util/kit.js
+++ util/kit.js
@@ -10,3 +10,46 @@ export function getPagination(total, offset, limit) {
     total
   }
 }
+
+export function getImageSizeWarnTip(imgWidth, imageHeight, adWidth, adHeight) {
+  if ((imageHeight < adHeight) || (imgWidth < adWidth)) {
+    return `图片尺寸较小, 会影响效果哟~ (广告位: ${adWidth}*${adHeight}, 图片: ${imgWidth}*${imageHeight})`
+  }
+  if ((imgWidth / imageHeight).toFixed(2) !== (adWidth / adHeight).toFixed(2)) {
+    return `图片尺寸比例与广告位不匹配, 会影响效果哟~ (广告位: ${adWidth}*${adHeight}, 图片: ${imgWidth}*${imageHeight})`
+  }
+  return ''
+}
+
+export function getImageInfo(file) {
+  if (typeof file === 'string') {
+    // file -> url
+    return new Promise((resolve, reject) => {
+      const img = new Image()
+      img.onload = () => {
+        resolve({
+          height: img.height,
+          width: img.width
+        })
+      }
+      img.src = file
+
+      setTimeout(() => reject(new Error('加载图片超时')), 3000)
+    })
+  }
+
+  return new Promise((resolve, reject) => {
+    // file -> file object
+    const URL = window.URL || window.webkitURL
+    const img = new Image()
+    img.onload = () => {
+      resolve({
+        height: img.height,
+        width: img.width
+      })
+    }
+    img.src = URL.createObjectURL(file)
+
+    setTimeout(() => reject(new Error('加载图片超时')), 2000)
+  })
+}
