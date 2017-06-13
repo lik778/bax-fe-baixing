0703f2337fb5c3e3d3873b8cd80c415d566ac904
haoxin <haoxins@outlook.com>
2017-06-13 20:16:57 +0800

    qwt-create-promotion - add keyword-table

---
+++ component/qwt-create-promotion/keyword-table.vue
@@ -0,0 +1,42 @@
+
+<template>
+  <div>
+    <el-table :data="data" style="width: 860px">
+      <el-table-column type="selection" width="40" />
+      <el-table-column prop="word" label="关键词" width="420" />
+      <el-table-column prop="count" label="日均搜索量" width="200" />
+      <el-table-column prop="price" label="CPC最高出价 (元/次点击)" width="200" />
+    </el-table>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'qwt-keyword-table',
+  // props: {
+  //   data: {
+  //     type: Array,
+  //     required: true
+  //   }
+  // }
+  data() {
+    return {
+      data: [{
+        word: '怎么撩妹',
+        count: 456,
+        price: 99
+      }, {
+        word: '棒妹棒棒哒 ~',
+        count: 567,
+        price: 99
+      }, {
+        word: '棒哥是怎么傍上棒妹的, 这是一个棒棒的谜 ~',
+        count: 678,
+        price: 99
+      }]
+    }
+  }
+}
+
+</script>
