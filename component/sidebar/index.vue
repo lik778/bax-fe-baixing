0c4c83bfefa2464a231c918aea84c0c0148bb44d
haoxin <haoxins@outlook.com>
2017-06-14 16:10:39 +0800

    sidebar - add 建站工具

--- component/sidebar/index.vue
+++ component/sidebar/index.vue
@@ -7,6 +7,21 @@
     </header>
     <content>
       <el-menu default-active="1">
+        <el-submenu index="1">
+          <template slot="title">
+            <i class="el-icon-star-on" />建站工具
+          </template>
+          <el-menu-item index="1-1">
+            <router-link :to="{ name: 'qwt-charge' }" tag="p">
+              企业官网
+            </router-link>
+          </el-menu-item>
+          <el-menu-item index="1-2">
+            <router-link :to="{ name: 'qwt-charge' }" tag="p">
+              单页定制
+            </router-link>
+          </el-menu-item>
+        </el-submenu>
         <el-submenu index="2">
           <template slot="title">
             <i class="el-icon-star-on" />全网通
