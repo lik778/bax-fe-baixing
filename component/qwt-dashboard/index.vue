664fb1add29bb6cbb3202399eefa2354979df778
haoxin <haoxins@outlook.com>
2017-06-14 11:54:34 +0800

    qwt-dashboard add topbar

--- component/qwt-dashboard/index.vue
+++ component/qwt-dashboard/index.vue
@@ -1,18 +1,36 @@
 
 <template>
   <div class="qwt-dashboard">
-    数据报表 - TODO
+    <topbar :user-info="userInfo">
+      <label slot="title">全网通 - 数据报表</label>
+    </topbar>
   </div>
 </template>
 
 <script>
 
+import Topbar from 'com/topbar'
+
 export default {
-  name: 'qwt-dashboard'
+  name: 'qwt-dashboard',
+  components: {
+    Topbar
+  },
+  props: {
+    userInfo: {
+      type: Object,
+      required: true
+    }
+  }
 }
 
 </script>
 
 <style scoped>
+
+.qwt-dashboard {
+  padding: 0 35px;
+  width: 100%;
+}
 
 </style>
