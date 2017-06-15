b5115af8e56a8d3cf385e98247f11a1df8553cb8
haoxin <haoxins@outlook.com>
2017-06-15 11:28:29 +0800

    constant++

---
+++ constant/fengming.js
@@ -0,0 +1,14 @@
+
+export const semPlatform = {
+  '0': 'baidu',
+  '1': 'qihu',
+  '3': 'baidu test',
+  '4': 'qihu test',
+  '5': 'sogou'
+}
+
+export const landingType = {
+  '0': '帖子详情页',
+  '1': '企业官网',
+  '2': '活动定制页'
+}
