b37fa2997def8a20bb93a5c41e944af3a5a0807d
haoxin <haoxins@outlook.com>
2017-06-19 16:56:30 +0800

    qwt-dashboard/data-detail ++

--- component/qwt-dashboard/data-detail.vue
+++ component/qwt-dashboard/data-detail.vue
@@ -4,16 +4,94 @@
     <header>
       数据详情
     </header>
+    <section>
+      <span>
+        <label>自定义列:</label>
+        <bax-select multiple :options="columnOpts"
+          v-model="displayColumns" />
+      </span>
+      <span>
+        <el-button icon="view" type="primary">
+          下载
+        </el-button>
+      </span>
+    </section>
     <main>
-      TODO
+      <el-table :data="data">
+        <el-table-column v-if="checkVisiable('channel')"
+          prop="channel" label="渠道" />
+        <el-table-column v-if="checkVisiable('platform')"
+          prop="platform" label="设备"  />
+        <el-table-column v-if="checkVisiable('keyword')"
+          prop="keyword" label="关键词" />
+        <el-table-column v-if="checkVisiable('impr')"
+          prop="impr" label="展现" />
+        <el-table-column v-if="checkVisiable('click')"
+          prop="click" label="点击"  />
+        <el-table-column v-if="checkVisiable('cost')"
+          prop="cost" label="消费" />
+        <el-table-column v-if="checkVisiable('percent')"
+          prop="percent" label="点击率" />
+      </el-table>
     </main>
   </div>
 </template>
 
 <script>
 
+import BaxSelect from 'com/common/select'
+
+// {
+//   label: '全选',
+//   value: '_all_'
+// }
+
+const columnOpts = [{
+  label: '渠道',
+  value: 'channel'
+}, {
+  label: '设备',
+  value: 'platform'
+}, {
+  label: '关键词',
+  value: 'keyword'
+}, {
+  label: '展现',
+  value: 'impr'
+}, {
+  label: '点击',
+  value: 'click'
+}, {
+  label: '消费',
+  value: 'cost'
+}, {
+  label: '点击率',
+  value: 'percent'
+}]
+
 export default {
-  name: 'qwt-dashboard-data-detail'
+  name: 'qwt-dashboard-data-detail',
+  components: {
+    BaxSelect
+  },
+  data() {
+    return {
+      displayColumns: columnOpts.map(c => c.value),
+      data: [{}, {}, {}, {}, {}],
+      columnOpts
+    }
+  },
+  methods: {
+    checkVisiable(column) {
+      const { displayColumns } = this
+
+      if (displayColumns.includes('_all_')) {
+        return true
+      }
+
+      return displayColumns.includes(column)
+    }
+  }
 }
 
 </script>
@@ -32,6 +110,30 @@ export default {
     background: #e5e9f2;
     font-size: 14px;
     color: #6a778c;
+  }
+
+  & > section {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    min-height: 68px;
+    padding: 10px 15px 10px 5px;
+
+    & > span:first-child {
+      display: flex;
+      align-items: center;
+      min-width: 480px;
+
+      & > label {
+        margin-right: 25px;
+        font-size: 14px;
+        color: #6a778c;
+      }
+
+      & > div {
+        flex-grow: 1;
+      }
+    }
   }
 }
 
