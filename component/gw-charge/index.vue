4ebc2061403f44ea9ff6b85b24852c9b73b4d629
haoxin <haoxins@outlook.com>
2017-06-19 14:51:00 +0800

    gw-charge - INIT

---
+++ component/gw-charge/index.vue
@@ -0,0 +1,36 @@
+
+<template>
+  <div class="gw-charge">
+    <topbar :user-info="userInfo">
+      <label slot="title">精品官网 - 购买</label>
+    </topbar>
+  </div>
+</template>
+
+<script>
+
+import Topbar from 'com/topbar'
+
+export default {
+  name: 'gw-charge',
+  components: {
+    Topbar
+  },
+  props: {
+    userInfo: {
+      type: Object,
+      required: true
+    }
+  }
+}
+
+</script>
+
+<style scoped>
+
+.gw-charge {
+  padding: 0 35px;
+  width: 100%;
+}
+
+</style>
