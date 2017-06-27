604e14c016c13c7b4f345162e86a445596ed6fe8
haoxin <haoxins@outlook.com>
2017-06-27 21:05:52 +0800

    config

--- config/index.js
+++ config/index.js
@@ -4,15 +4,15 @@ const isPro = location.hostname.includes('bax.baixing.com.cn')
 export const qiniuHost = '//ono2r7ueh.bkt.clouddn.com/'
 
 export const fengmingApiHost = isPro
-  ? '//fengming.baixing.com.cn/api'
+  ? '//bax.baixing.com.cn/fengmingx-api'
   : '//192.168.8.201/fengmingx'
 
 export const baxApiHost = isPro
-  ? '//bax.baixing.com.cn/api'
+  ? '//bax.baixing.com.cn/bax-api'
   : '//192.168.8.201/bax'
 
 export const dashboardHost = isPro
-  ? '//bax.baixing.com.cn/api/dashboard'
+  ? '//bax.baixing.com.cn/dashboard-api'
   : '//localhost:3003/api'
 
 export const assetHost = '//adfe.wzbaixing.com.cn/bax-asset.'
