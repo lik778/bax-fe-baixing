c59b379e2e5f919cdb8ec2dd0d219da880260a9e
haoxin <haoxins@outlook.com>
2017-08-21 17:19:46 +0800

    sm fix

--- component/widget/promotion-charge-tip.vue
+++ component/widget/promotion-charge-tip.vue
@@ -14,7 +14,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 footer {
   color: #717d91;
--- component/widget/promotion-mobile-ratio-tip.vue
+++ component/widget/promotion-mobile-ratio-tip.vue
@@ -12,7 +12,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 
 footer {
   margin: 10px 0;
