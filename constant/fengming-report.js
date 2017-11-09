5b0e384be0b067efeaf60b1df8179fc3c86959ce
haoxin <haoxins@outlook.com>
2017-11-09 16:00:47 +0800

    constant ++

--- constant/fengming-report.js
+++ constant/fengming-report.js
@@ -34,3 +34,44 @@ export const allTimeUnits = [{
   label: '每年',
   value: 4
 }]
+
+export const columnOpts = [{
+  label: '日期',
+  value: 'date'
+}, {
+  label: '推广计划',
+  value: 'campaignId'
+}, {
+  label: '渠道',
+  value: 'channel' // 暂缺
+}, {
+  label: '设备',
+  value: 'device'
+}, {
+  label: '关键词',
+  value: 'keyword' // 暂缺
+}, {
+  label: '展现',
+  value: 'shows'
+}, {
+  label: '点击',
+  value: 'clicks'
+}, {
+  label: '点击均价',
+  value: 'clickAvgPrice'
+}, {
+  label: '消费',
+  value: 'cost'
+}, {
+  label: '点击率',
+  value: 'clickRate'
+}, {
+  label: '平均排名',
+  value: 'cpcRanking'
+}]
+
+export const fields = columnOpts.map(col => {
+  return col.value.replace(/[A-Z]/g, (s) => {
+    return '_' + s.toLowerCase()
+  })
+})
