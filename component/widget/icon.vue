f0ebc092845299e34be5439150b5ea61380c7bb1
haoxin <haoxins@icloud.com>
2018-01-29 23:33:10 +0800

    add widget - icon

---
+++ component/widget/icon.vue
@@ -0,0 +1,15 @@
+<template>
+  <i :class="'iconfont icon-' + type"></i>
+</template>
+
+<script>
+export default {
+  name: 'qwt-bx-icon',
+  props: {
+    type: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
