b6e8dd918b8089c4ef6da8add10b3904d060d2a9
haoxin <haoxins@icloud.com>
2018-06-04 17:53:21 +0800

    .

--- component/widget/creative-editor.vue
+++ component/widget/creative-editor.vue
@@ -1,8 +1,56 @@
 <template>
   <section class="editor">
     <promotion-creative-tip />
+    <div>
+      <aside>推广标题:</aside>
+      <span>
+        <text-limit-tip
+          :rest="25 - title.length"
+          :current="title.length"
+          :min="9">
+          <el-input slot="input" type="text" style="width: 420px"
+            placeholder="请输入标题 ~ (字数限制为9-25个字)"
+            :disabled="!isCreativeEditable || isFormReadonly"
+            :value="title"
+            @change="onChangeTitle"
+          />
+        </text-limit-tip>
+        <p
+          v-if="titleTip"
+          class="authing-tip"
+        >
+          {{ titleTip }}
+        </p>
+      </span>
+    </div>
+    <div>
+      <aside style="align-items: flex-start; padding-top: 5px;">
+        推广内容:
+      </aside>
+      <span>
+        <text-limit-tip
+          :rest="40 - content.length"
+          :current="content.length"
+          :min="9">
+          <el-input slot="input" type="textarea"
+            :rows="5" style="width: 420px"
+            :placeholder="creativeContentPlaceholder"
+            :disabled="!isCreativeEditable || isFormReadonly"
+            :value="content"
+            @change="onChangeContent"
+          />
+        </text-limit-tip>
+      </span>
+      <p class="authing-tip-2">
+        {{ refuseReason }}
+      </p>
+    </div>
     <footer v-if="!isFormReadonly">
       <el-button
+        :disabled="checkCreativeBtnDisabled"
+        type="primary"
+        @click="checkCreativeContent"
+      >
         检查推广是否可用
       </el-button>
     </footer>
@@ -30,3 +78,116 @@ export default {
     TextLimitTip
   },
   props: {
+    checkCreativeBtnDisabled: {
+      type: Boolean,
+      default: false
+    },
+    isCreativeEditable: {
+      type: Boolean,
+      default: true
+    },
+    isFormReadonly: {
+      type: Boolean,
+      default: false
+    },
+    refuseReason: {
+      type: String,
+      default: ''
+    },
+    titleTip: {
+      type: String
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    content: {
+      type: String,
+      required: true
+    },
+    platform: {
+      type: Number,
+      required: true
+    }
+  },
+  data() {
+    return {
+      creativeContentPlaceholder
+    }
+  },
+  methods: {
+    onChangeTitle(v) {
+      this.$emit('change-title', v)
+    },
+    onChangeContent(v) {
+      this.$emit('change-content', v)
+    },
+    async checkCreativeContent() {
+      const {
+        platform,
+        content,
+        title
+      } = this
+
+      if (!content) {
+        return Message.error('请填写推广内容')
+      }
+
+      if (!title) {
+        return Message.error('请填写推广标题')
+      }
+
+      const data = await checkCreativeContent({
+        creativeContent: content,
+        creativeTitle: title,
+        platform
+      })
+
+      if (!data.result) {
+        Message.success(data.hint)
+      } else {
+        return Message.error(data.hint)
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.authing-tip {
+  display: inline-flex;
+  align-items: center;
+  font-size: 12px;
+  color: #ff4401;
+}
+
+.authing-tip-2 {
+  margin-left: 5px;
+  font-size: 12px;
+  line-height: 1.75;
+  color: #ff4401;
+}
+
+.editor {
+  margin-bottom: 30px;
+  padding-bottom: 10px;
+
+  & > header {
+    color: #6a778c;
+  }
+
+  & > div {
+    display: flex;
+    margin: 20px 0;
+
+    & > aside:first-child {
+      display: flex;
+      align-items: center;
+      min-width: 70px;
+      margin-right: 20px;
+      color: #6a778c;
+      font-size: 14px;
+    }
+  }
+}
+</style>
