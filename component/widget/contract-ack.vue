5e66b017432380d647329bd461f2028dbd368dd7
zbl91555 <zbl91555@163.com>
2018-12-13 11:54:32 +0800

    feat: update promotion contract

--- component/widget/contract-ack.vue
+++ component/widget/contract-ack.vue
@@ -3,14 +3,14 @@
     <el-checkbox :value="true" />
     <label>我已阅读并同意遵守</label>
     <a v-if="type === 'contract'"
-      v-bind:href="assetHost + 'baixing-qwt-promotion-contract.docx'"
-      download="百姓网站外推广用户协议.docx">
-      《百姓网站外推广用户协议》
+      v-bind:href="assetHost + 'baixing-qwt-contract.docx'"
+      download="百姓网全网通用户协议.docx">
+      《百姓网全网通用户协议》
     </a>
     <a v-if="type === 'content-rule'"
       v-bind:href="assetHost + 'baixing-online-promotion-content-rule.docx'"
       download="百姓网站外推广填写准则.docx">
-      《百姓网站外推广填写准则》
+      《百姓网填写准则》
     </a>
   </h4>
 </template>
