5d677dbd4df04d2176c85b961f92e19593ab9cd2
zbl91555 <zbl91555@163.com>
2019-01-08 14:55:17 +0800

    fix: fix bug

--- component/qwt-promotion-list/index.vue
+++ component/qwt-promotion-list/index.vue
@@ -271,7 +271,7 @@ export default {
     queryParams: {
       deep: true,
       handler(val) {
-        if (/^[0-9]+$/.test(val.campaignId)) {
+        if (!val.campaignId || /^[0-9]+$/.test(val.campaignId)) {
           this.fetchlandingPageList()
         } else {
           this.$message.error('您要查询的计划id格式不正确')
