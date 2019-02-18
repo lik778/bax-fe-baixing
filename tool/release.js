536224b31c4b2a54245428d22972c95d94ef99d5
Tang,Hejun <tanghejun@baixing.com>
2019-02-18 11:37:07 +0800

    update release script

--- tool/release.js
+++ tool/release.js
@@ -6,19 +6,33 @@ const fs = require('fs')
 const assets = fs.readdirSync(join(__dirname, '../dist'))
 const pages = fs.readdirSync(join(__dirname, '../view'))
 
-pages.forEach((page) => {
-  page = join(__dirname, '../view', page)
-  let text = fs.readFileSync(page, 'utf-8')
+console.log(assets)
 
-  assets.forEach((asset) => {
-    const [name, _, ext, map] = asset.split('.')
-    if (map) {
-      return
-    }
+for (const asset of assets) {
+  const [name, _, ext] = asset.split('.')
+  console.log(asset, name, _, ext)
+  if (ext === 'html' || _ === 'html') {
+    fs.renameSync(
+      join(__dirname, '../dist/', asset),
+      join(__dirname, '../view/', asset)
+    )
+  }
+}
 
-    const reg = new RegExp(name + '.\\w+.' + ext)
-    text = text.replace(reg, asset)
-  })
 
-  fs.writeFileSync(page, text)
-})
+// pages.forEach((page) => {
+//   page = join(__dirname, '../view', page)
+//   let text = fs.readFileSync(page, 'utf-8')
+
+//   assets.forEach((asset) => {
+//     const [name, _, ext, map] = asset.split('.')
+//     if (map) {
+//       return
+//     }
+
+//     const reg = new RegExp(name + '.\\w+.' + ext)
+//     text = text.replace(reg, asset)
+//   })
+
+//   fs.writeFileSync(page, text)
+// })
