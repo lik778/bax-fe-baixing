70377a8e06aac7a9a320ee3bd614fdab24951d85
zbl91555 <zbl91555@163.com>
2019-10-17 13:22:31 +0800

    add 线下测试账户

--- util/fengming-role.js
+++ util/fengming-role.js
@@ -22,7 +22,7 @@ export function allowSee258(roles, uid) {
 export function allowUseKaPackage(roles, uid) {
   // 860603 优米帮
   // 318 康品汇
-  const specialKaUserIds = [860603, 318, 47709, 53470] // 这几个比较牛逼, 特价 (1000) 买官网
+  const specialKaUserIds = [860603, 318, 47709, 53470, 93] // 这几个比较牛逼, 特价 (1000) 买官网
   return specialKaUserIds.includes(uid)
 }
 
