feb7c8baf161148342b7ef0c3acfe612aad6d3a4
zbl91555 <zbl91555@163.com>
2019-12-04 16:01:28 +0800

    fix: fix bw charge

--- component/common/bw-shopping-cart.vue
+++ component/common/bw-shopping-cart.vue
@@ -185,8 +185,7 @@
         const {salesId, userId} = salesInfo
         let createOrderArgs = [localItems, userId, salesId]
         if (gwSelected.length) {
-          const { shopOrderAmount: oriShopOrderAmount, shopType } = siteProducts.find(({id}) => id === gwSelected[0])
-          const shopOrderAmount =  shopType === 2 ? oriShopOrderAmount * 2 : oriShopOrderAmount
+          const { shopOrderAmount, shopType } = siteProducts.find(({id}) => id === gwSelected[0])
           createOrderArgs = createOrderArgs.concat([true, shopOrderAmount, shopType])
         } else {
           // 不搭售官网 saleWithShopOrder false
