fe9c517221f61ff3186eb94e96cead53733e8d4d
zbl91555 <zbl91555@163.com>
2019-12-13 16:07:42 +0800

    Revert "Merge branch 'feature-1212' into 'master'"
    This reverts commit 2c4eef6bfa467bbec5c6728adfa8f3e4e475c4ed, reversing
    changes made to 53f37af17f05c04cadf82efb9efc438c7fc60952.

--- tool/webpack.pro.js
+++ tool/webpack.pro.js
@@ -53,7 +53,7 @@ config.plugins = [
   new HtmlWebpackPlugin({
     filename: 'main.html',
     template: 'template/main.html',
-    chunks: ['vendors~bax~huodong~signin', 'vendors~bax~signin', 'bax'],
+    chunks: ['vendors~bax~signin', 'vendors~bax', 'bax'],
     minify: {
       collapseWhitespace: true,
     }
@@ -61,19 +61,11 @@ config.plugins = [
   new HtmlWebpackPlugin({
     filename: 'signin.html',
     template: 'template/signin.html',
-    chunks: ['vendors~bax~huodong~signin', 'vendors~bax~signin', 'signin'],
+    chunks: ['vendors~bax~signin', 'signin'],
     minify: {
       collapseWhitespace: true,
     }
   }),
-  new HtmlWebpackPlugin({
-    filename: 'huodong.html',
-    template: 'template/huodong.html',
-    chunks: ['vendors~bax~huodong~signin', 'huodong'],
-    minify: {
-      collapseWhitespace: true,
-    }
-  })
   // new BundleAnalyzerPlugin()
 ]
 
