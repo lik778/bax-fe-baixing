b512d7b5c006111439a347ab65e6c86e15d7783a
zbl91555 <zbl91555@163.com>
2020-03-12 11:41:21 +0800

    活动页配置项

---
+++ component/huodong/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    活动页
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'active-page'
+}
+</script>
+
+<style lang="postcss" scoped>
+@import "../../cssbase/var.css";
+</style>
