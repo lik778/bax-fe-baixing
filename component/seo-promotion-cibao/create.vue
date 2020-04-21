b3ed0b1eebc65506301a8843a230de1fffef7f07
xielizhen <xielizhen@baxing.com>
2020-04-21 12:02:47 +0800

    文案修改

--- component/seo-promotion-cibao/create.vue
+++ component/seo-promotion-cibao/create.vue
@@ -8,7 +8,7 @@
         :product-type="PRO_SITE_PRODUCT_TYPE"
       />
       <p class="warning" v-if="showExpireWarning">
-        站点{{promotion.duration === NINETY_DAYS ? '3' :' 6'}}个月内将过期，
+        站点{{promotion.duration}}天内将过期，
         请选择其他站点，或<router-link :to="{name: 'seo-charge'}">购买</router-link>新官网
       </p>
       <p class="warning" v-if="showExistWebsite">该站点已创建首页宝加速词包计划，请更换</p>
