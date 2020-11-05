9f9769dddecd783a3c6bb0427a7b2d4dc3811d27
wulei <wulei@baixing.com>
2020-11-05 10:24:32 +0800

    feat: 将B,D词上限修改为20个词

--- component/qc-create-promote/select-keywords/keyword-options.js
+++ component/qc-create-promote/select-keywords/keyword-options.js
@@ -9,7 +9,6 @@ export default {
     inputTitle: '添加服务城市',
     info: `<p>可通过“城市选择”添加服务城市，也可以在输入框中自填地域；</p>
            <p>多个关键词换行、中英文逗号隔开；</p>
-           <p>词数不少于15个，不超过500个字；</p>
            <p>单个词长度不少于2个字，不超过8个字。</p>`,
     placeholder: '如:，上海，闵行区，徐汇区，七莘路，七宝镇，...',
     keywords: [],
@@ -23,14 +22,14 @@ export default {
     title: '前缀词（B类词）',
     inputTitle: '添加前缀词',
     info: `<p>多个关键词换行、中英文逗号隔开；</p>
-           <p>词数不少于10个，不超过100个字；</p>
-           <p>单个词长度不少于2个字，不超过8个字。</p>
-           <p>百度投放仅支持小写，输入大写时数据会被强制更改为小写</p>`,
+           <p>词不少于10个不超过20个；</p>
+           <p>单个词长度不少于2个字，不超过8个字；</p>
+           <p>百度投放仅支持小写，输入大写时数据会被强制更改为小写。</p>`,
     placeholder:
       '如：，A380、工业、民用、塑料、不锈钢、金属、进口、国产、靠谱、专业、……',
     keywords: [],
     keywordsAlias: 'prefixWordList',
-    wordsLimit: [10, 100],
+    wordsLimit: [10, 20],
     wordLenLimit: [2, 8],
     isEdit: true
   },
@@ -52,13 +51,13 @@ export default {
     title: '后缀词（D类词）',
     inputTitle: '添加后缀词',
     info: `<p>多个关键词换行、中英文逗号隔开；</p>
-           <p>词数不少于10个，不超过100个字；</p>
-           <p>单个词长度不少于2个字，不超过8个字。</p>
-           <p>百度投放仅支持小写，输入大写时数据会被强制更改为小写</p>`,
+           <p>词不少于10个不超过20个；</p>
+           <p>单个词长度不少于2个字，不超过8个字；</p>
+           <p>百度投放仅支持小写，输入大写时数据会被强制更改为小写。</p>`,
     placeholder: '如：，价格、厂商、厂家、批发、多少钱、怎么样、……',
     keywords: [],
     keywordsAlias: 'suffixWordList',
-    wordsLimit: [10, 100],
+    wordsLimit: [10, 20],
     wordLenLimit: [2, 8],
     isEdit: true
   }
