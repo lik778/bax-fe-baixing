859b81ea1735a3abaafa3b5924af23c7406f4c1d
xielizhen <xielizhen@baixing.com>
2021-06-07 14:15:42 +0800

    feat: 618活动和图标调整

--- component/common/huodong-btn.vue
+++ component/common/huodong-btn.vue
@@ -1,13 +1,14 @@
 <template>
-  <div class="huodong-btn">
-    <a target="_blank" :href="href" >
+  <div class="huodong-btn" v-if="visible">
+    <a target="_blank" :href="href">
       <img src="//file.baixing.net/202106/05873a4a3b73a382de36ed49a76d2fed.png">
     </a>
+    <i class="el-icon-close" @click="visible = false"></i>
   </div>
 </template>
 
 <script>
-const href = 'https://www.baixing.com/rev/lottery?activity=lottery_activity16thAnniversary'
+const href = 'https://www.baixing.com/rev/lottery?activity=lottery_activity20210618'
 
 export default {
   name: 'huodong-btn',
@@ -37,6 +38,14 @@ export default {
   height: 116px;
   cursor: pointer;
   z-index: 100;
+  .el-icon-close {
+    position: absolute;
+    top: -6px;
+    right: 0;
+    color: #666;
+    border-radius: 50%;
+    font-size: 14px;
+  }
 }
 .huodong-btn img {
   width: 100%;
--- constant/activity.js
+++ constant/activity.js
@@ -22,20 +22,15 @@ export const fengmingDiscountInfo = {
   },
   [ACTIVITY_STATUS]: { // 活动文案展示(可替换)
     discountInfo: [
-      [1000, 200, 600, 600],
-      [3088, 200, 600, 600],
-      [5088, 600, 1200, 900],
-      [6088, 600, 1200, 900, 188],
-      [10188, 1000, 1400, 1500, 388],
-      [20000, 1000, 1400, 1500, 888]
+      [1000, 200, 600],
+      [5000, 600, 1200],
+      [9888, 1200, 2000]
     ],
     get discountInfoHTML () {
       return this.discountInfo.map((item) => {
-        return `满<span style="min-width:40px">${item[0]}</span>元：
-          ${item[4] ? `<span class="red">赠</span>送${item[4]}元凤鸣资金；` : ''}
-          同时购买精品官网（1年）官网<span class="red">减</span>立减 ${item[1]} 元；
-          购买精品官网2年【送一年】官网<span class="red">减</span>立减 ${item[2]} 元；
-          购买精品官网专业版1年官网<span class="red">减</span> ${item[3]}元；</p>`
+        return `<p class="discount-info">站外资金满 ${item[0]} 元：
+        同时购买 1 年钻石店铺，<i class="red">减</i>立减 ${item[1]} 元；
+        同时购买 2 年钻石店铺，送 1 年钻石店铺时长，<i class="red">减</i>立减 ${item[2]} 元；</p>`
       })
     }
   }
@@ -54,12 +49,14 @@ export const biaowangDiscountInfo = {
     }
   },
   [ACTIVITY_STATUS]: { // 活动文案
+    discountInfo: [
+      [1000, 200, 600],
+      [5000, 600, 1200],
+      [9888, 1200, 2000]
+    ],
     get discountInfoHTML () {
-      return [`<div class="notice">
-        <p><span><i class="red">满</i>500-4999元，</span>同时购买精品官网1年送1年 标准版再<i class="red">减</i>200元/ 专业版再<i class="red">减</i>600 元；</p>
-        <p><span><i class="red">满</i>5000-9999元，</span>同时购买精品官网1年送1年 标准版再<i class="red">减</i>600元/ 专业版再<i class="red">减</i>900 元；</p>
-        <p><span><i class="red">满</i>10000元及以上，</span>同时购买精品官网1年送1年 标准版再<i class="red">减</i>1000元/ 专业版再<i class="red">减</i>1500 元；</p>
-      </div>`]
+      const genHTML = item => `<p class="discount-info">标王资金满 ${item[0]} 元：同时购买 1 年钻石店铺，<i class="red">减</i>立减 ${item[1]} 元；同时购买 2 年钻石店铺，送 1 年钻石店铺时长，<i class="red">减</i>立减 ${item[2]} 元；</p>`
+      return this.discountInfo.map(item => genHTML(item))
     }
   }
 }
