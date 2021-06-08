ce650c68370d9ea48a0bbcdb2abf66b34452d373
xielizhen <xielizhen@baixing.com>
2021-06-08 15:21:34 +0800

    fix: 无法选中问题

--- component/common/ad-selector.vue
+++ component/common/ad-selector.vue
@@ -177,11 +177,14 @@ export default {
       }
     },
     async onSelectAd (notEmitSelected) {
+      console.log('onSelectAd')
+      console.log(this.checkedAdId)
       if (!this.checkedAdId) {
         return
       }
       if (!notEmitSelected) {
         const ad = this.ads.find(ad => ad.adId === this.checkedAdId)
+        console.log(ad)
         this.$emit('select-ad', {
           ...ad,
           url: isArray(ad.url) ? ad.url[0] : ad.url
@@ -214,7 +217,6 @@ export default {
       this.offset = 0
       this.mode = MODE_INIT
       this.keyword = ''
-      this.$emit('select-ad', { url: '', adId: '' })
 
       await this.queryAds()
     },
