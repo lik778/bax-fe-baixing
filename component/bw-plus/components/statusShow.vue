c02bcc670faf5e394bd7e68cd7c4d2daca2e9ac5
xielizhen <xielizhen@baixing.com>
2021-07-12 11:30:44 +0800

    feat: 管理推广页面完成

---
+++ component/bw-plus/components/statusShow.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+     <span :class="['dot', dotColor]" />
+     <span>{{txt}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'statusShow',
+  props: {
+    dotColor: {
+      type: String,
+      required: false
+    },
+    txt: {
+      type: String,
+      required: false
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.dot {
+  border-radius: 50%;
+  width: 6px;
+  height: 6px;
+  &.info {
+    background: $c-ignore;
+  }
+  &.success {
+    background: $c-success;
+  }
+  &.warning {
+    background: $c-strong;
+  }
+}
+</style>
