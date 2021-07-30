31f36cc59e98cb0bfbaf2850348dfd45675538d3
zhongyuanyuan <zhongyuanyuan@baixing.com>
2021-07-30 14:39:29 +0800

    fix:优化师变身销售进入去掉cookie的source

--- component/bax.vue
+++ component/bax.vue
@@ -55,6 +55,7 @@ import {
   normalizeRoles,
   isSales
 } from 'util/role'
+import { delCookie } from 'util/cookie'
 
 import { router } from '../template/bax'
 import qs from 'query-string'
@@ -139,8 +140,11 @@ export default {
   },
   async mounted () {
     const { source } = qs.parse(location.search)
-    console.log('source', source)
-    document.cookie = document.cookie + 'source=' + source + ';'
+    if (source) {
+      document.cookie = 'source=' + source + ';'
+    } else {
+      delCookie('source')
+    }
     await Promise.all([
       gStore.getCurrentUser(),
       gStore.getCategories(),
