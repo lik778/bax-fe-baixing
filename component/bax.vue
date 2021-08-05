9203d401bbd413e32936091169d5512d9a5d4a7a
zhongyuanyuan <zhongyuanyuan@baixing.com>
2021-08-05 14:52:46 +0800

    fix：被迫加一些莫名其妙的逻辑

--- component/bax.vue
+++ component/bax.vue
@@ -59,6 +59,7 @@ import { delCookie } from 'util/cookie'
 
 import { router } from '../template/bax'
 import qs from 'query-string'
+import { getUserAuthRelation } from 'api/fengming'
 
 export default {
   name: 'bax',
@@ -139,9 +140,12 @@ export default {
     }
   },
   async mounted () {
+    // source为当前用户是否是以优化师角色进入bax
     const { source } = qs.parse(location.search)
     if (source) {
       document.cookie = 'source=' + source + ';'
+      // 此接口为查询当前用户的角色（主管/优化师）以及和目标用户的关系，后端暂存，用作后面的校验
+      await getUserAuthRelation()
     } else {
       delCookie('source')
     }
