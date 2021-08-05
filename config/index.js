e7a18a385c73dae4a10675766563f7ab72418eb7
zhongyuanyuan <zhongyuanyuan@baixing.com>
2021-08-05 11:43:28 +0800

    fix:修改接口域名

--- config/index.js
+++ config/index.js
@@ -29,7 +29,7 @@ const development = {
 }
 
 const local = {
-  fengmingApiHost: `//${window.location.host}/testApi/fengmingx`,
+  fengmingApiHost: '//bax.baixing.cn/fengmingx',
   baxApiHost: '//bax.baixing.cn/bax',
   kaApiHost: '//bax.baixing.cn/ka/api/admin',
   biaowangApiHost: '//bax.baixing.cn/phoenixs/api',
