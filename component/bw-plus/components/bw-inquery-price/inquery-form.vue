a52e185fbf801c28b18377d6a7976845d323d25a
zhongyuanyuan <zhongyuanyuan@baixing.com>
2021-08-11 16:03:02 +0800

    fix:关键词个数限制

--- component/bw-plus/components/bw-inquery-price/inquery-form.vue
+++ component/bw-plus/components/bw-inquery-price/inquery-form.vue
@@ -152,10 +152,10 @@ export default {
         }
         return errorMsg
       }
-      if (array.length > 30) {
+      if (array.length > 10) {
         errorMsg = {
           validate: false,
-          error: '关键词个数不能超过30个'
+          error: '关键词个数不能超过10个'
         }
         return errorMsg
       }
