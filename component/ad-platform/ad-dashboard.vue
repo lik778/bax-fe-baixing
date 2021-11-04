c8d22e044a6382c26d08fbcb1d17be33ec2c9b40
zhongyuanyuan <zhongyuanyuan@baixing.com>
2021-11-04 18:33:13 +0800

    feat:报表

--- component/ad-platform/ad-dashboard.vue
+++ component/ad-platform/ad-dashboard.vue
@@ -1,26 +1,27 @@
 <template>
-    <section class="ad-platform-dashboard">
-        <h3 class="title">资金概览</h3>
-        <ul>
-            <li>
-                <span>今日消耗（元）</span>
-                <span>88</span>
-            </li>
-            <li>
-                <span>账户余额（元）</span>
-                <span>88</span>
-            </li>
-            <li>
-                <span>累计充值（元）</span>
-                <span>88</span>
-            </li>
-        </ul>
-        <h3 class="title">数据概览</h3>
-        <DataRange @getDate="getDate" @searchData="changeDateRange"/>
-    </section>
+  <section class="ad-platform-dashboard">
+    <h3 class="title">资金概览</h3>
+    <ul>
+      <li>
+        <span>今日消耗（元）</span>
+        <span>88</span>
+      </li>
+      <li>
+        <span>账户余额（元）</span>
+        <span>88</span>
+      </li>
+      <li>
+        <span>累计充值（元）</span>
+        <span>88</span>
+      </li>
+    </ul>
+    <h3 class="title">数据概览</h3>
+    <DataRange @getDate="getDate" @searchData="changeDateRange"/>
+  </section>
 </template>
 <script>
 import { DataRange } from 'com/bw-plus/components'
+import { statistic, costList, foundsInfo } from 'api/ad-platform'
 export default {
   name: 'ad-dashboard',
   components: {
@@ -28,14 +29,39 @@ export default {
   },
   data () {
     return {
-      daterange: []
+      daterange: [],
+      founds: {},
+      statistic: {},
+      costList: []
     }
   },
   methods: {
     getDate (daterange) {
       this.daterange = daterange
     },
-    changeDateRange () {}
+    changeDateRange () {},
+    async getFounds () {
+      const { data } = await foundsInfo()
+      this.founds = data
+    },
+    async getCostList () {
+      const { daterange } = this
+      const params = {
+        startDate: daterange[0].format('YYYY-MM-DD'),
+        endDate: daterange[1].format('YYYY-MM-DD')
+      }
+      const { data } = await costList(params)
+      this.costList = data
+    },
+    async getStatistic () {
+      const { daterange } = this
+      const params = {
+        startDate: daterange[0].format('YYYY-MM-DD'),
+        endDate: daterange[1].format('YYYY-MM-DD')
+      }
+      const { data } = await statistic(params)
+      this.statistic = data
+    }
   }
 }
 </script>
