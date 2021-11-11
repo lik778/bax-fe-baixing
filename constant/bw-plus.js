88c8f33914d3c15d6d1d9c6749dfc2feeeeeb2d7
zhongyuanyuan <zhongyuanyuan@baixing.com>
2021-11-11 11:30:12 +0800

    fix:修改时间

--- constant/bw-plus.js
+++ constant/bw-plus.js
@@ -228,7 +228,7 @@ export const RENEW_OPRATION_STATUS_COPY = 30
 export const RENEW_OPRATION_STATUS_DISABLED = 60
 const showWelfare = () => {
   const now = dayjs()
-  return dayjs(now).isBetween('2021-11-1', dayjs('2021-11-11'))
+  return dayjs(now).isBetween('2021-11-1', dayjs('2021-11-12'))
 }
 
 export const welfareInfo = [
