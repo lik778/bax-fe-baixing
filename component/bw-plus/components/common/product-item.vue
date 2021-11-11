a6282cf36ff0eba29dd091ca906e3dcd0abf294f
zhongyuanyuan <zhongyuanyuan@baixing.com>
2021-11-11 11:28:45 +0800

    feat:UI

--- component/bw-plus/components/common/product-item.vue
+++ component/bw-plus/components/common/product-item.vue
@@ -4,6 +4,11 @@
             <span>{{product.name}}</span>
             <i class="hot-icon"></i>
         </h3>
+        <ul>
+            <li>360天</li>
+            <li>双端</li>
+            <li>7天*24小时</li>
+        </ul>
     </div>
 </template>
 <script>
@@ -21,11 +26,20 @@ export default {
 <style lang="scss" scoped>
 .product-wrapper{
     width: 20%;
+    border: 1px solid #FA6A78;
+    border-radius: 8px;
+    overflow: hidden;
+    box-sizing: border-box;
 }
 .product-title{
     display: flex;
     justify-content: space-between;
     align-items: center;
+    background-image: linear-gradient(180deg, #FFAD94 0%, #F95771 100%);
+    padding: 7px 10px;
+    font-family: PingFangSC-Medium;
+    font-size: 13px;
+    color: #FFFFFF;
     .hot-icon{
         width: 13px;
         height: 15px;
