296598d9297b7f96b9d6cd091c05702f53f7fc02
meeting <meeting@meetingdeMacBook-Air.local>
2021-11-29 15:50:02 +0800

    fix:热度为空

--- component/bw-plus/components/bw-inquery-price/error-footer.vue
+++ component/bw-plus/components/bw-inquery-price/error-footer.vue
@@ -1,7 +1,7 @@
 <template>
     <section class="footer">
-        <p v-if="!queryResult.error && queryResult.overHeat">{{queryResult.overHeatWords.join("、")}}热度>2000，暂无报价</p>
-        <p v-if="queryResult.error && !queryResult.overHeat">{{queryResult.overHeatWords.join("、")}}未获取到热度，请重试</p>
+        <p v-if="!queryResult.error && queryResult.overHeat">{{queryResult.overHeatWords.join("、")}}PV过热，暂不支持购买</p>
+        <p v-if="queryResult.error && !queryResult.overHeat">{{queryResult.errorWords.join("、")}}未获取到热度，请重试</p>
         <p v-if="queryResult.error && queryResult.overHeat">{{queryResult.overHeatWords.join("、")}}热度>2000，{{queryResult.errorWords.join("、")}}未获取到热度</p>
         <p v-if="queryResult.industryError">{{queryInfo.industryCn}}太热，暂无报价</p>
         <!-- <el-button @click="isSubmit = true" style="margin-top: 30px" type="danger">申请人工报价</el-button> -->
