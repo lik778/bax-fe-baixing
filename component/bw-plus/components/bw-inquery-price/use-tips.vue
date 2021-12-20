ec0cc522430d492bd13377c47dbbe34cef8c5160
meeting <meeting@meetingdeMacBook-Air.local>
2021-12-20 14:26:23 +0800

    fix:使用手册

--- component/bw-plus/components/bw-inquery-price/use-tips.vue
+++ component/bw-plus/components/bw-inquery-price/use-tips.vue
@@ -1,6 +1,6 @@
 <template>
     <el-tooltip class="item" effect="dark" content="点击下载使用手册" placement="top-start">
-        <a class="use-tips" href="http://file.baixing.net/bax-fe/static/product-use.pdf" download="产品使用手册">
+        <a class="use-tips" target="_blank" href="http://file.baixing.net/bax-fe/static/product-use.pdf" download="产品使用手册">
           <i class="el-icon-question"></i>
           使用指引
         </a>
