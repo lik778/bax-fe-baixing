33c16bffe4ac71ca7bd720d2cc608c3cab3354dc
libertas <zhongyuanyuan@baixing.com>
2022-01-13 16:16:13 +0800

    feat: 续费升级

--- component/bw-plus/renew-upgrade/index.vue
+++ component/bw-plus/renew-upgrade/index.vue
@@ -1,9 +1,14 @@
 <template>
-    <div>续费</div>
+    <div class="bw-plus-renew-upgrade">
+        <Header>续费</Header>
+    </div>
 </template>
 
 <script>
+import Header from 'com/bw-dashboard/header.vue'
 export default {
+  components: { Header },
   name: 'renew-upgrade'
 }
 </script>
+<style lang="scss" scoped src="./index.scss" />
