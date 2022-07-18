5824bb92d2e8c0d227e806148d62827ce039573b
likuan <likuan@baixing.com>
2022-07-18 16:49:39 +0800

    fix； 初始化2

---
+++ component/yibaisou-fengming/components/sidebar/index.vue
@@ -0,0 +1,236 @@
+
+<template>
+  <div class="sidebar">
+    <main>
+      <el-menu
+        style="border: none;"
+        :default-active="defaultActive"
+        :default-openeds="defaultOpeneds"
+        :unique-opened="true"
+        background-color="#fff"
+        active-text-color="#FF6350"
+        text-color="#333"
+        ref="menu"
+      >
+        <el-submenu index="bwPlus">
+          <template slot="title">
+            <bx-icon type="sharealt"></bx-icon>百搜凤鸣
+          </template>
+          <el-menu-item v-if="allowSeeQueryPrice" index="bw-plus-query-price">
+            <router-link :to="{ name: 'bw-plus-query-price' }" tag="p">
+              百搜标王查价
+            </router-link>
+          </el-menu-item>
+          <el-menu-item v-if="allowSeeRecord" index="bw-plus-price-records">
+            <router-link :to="{ name: 'bw-plus-price-records' }" tag="p">
+              查价记录
+            </router-link>
+          </el-menu-item>
+          <el-menu-item v-if="allowSeeManage" index="bw-plus-package-list">
+            <router-link :to="{ name: 'bw-plus-package-list' }" tag="p">
+              管理推广
+            </router-link>
+          </el-menu-item>
+        </el-submenu>
+        <el-menu-item v-if="allowSeeDianpu" index="diamond-site-homepage" key="diamond-site-homepage">
+          <a v-if="isDiamondSiteJumpToMainSite" href="//shop.baixing.com/management/shop" style="color: inherit">
+            <i class="el-icon-news" />钻石店铺
+          </a>
+          <router-link v-else :to="{ name: 'qwt-charge' }" tag="p">
+            <i class="el-icon-news" />钻石店铺
+          </router-link>
+        </el-menu-item>
+        <el-submenu v-if="allowSeeDianpu" index="dashboard">
+          <template slot="title">
+            <i class="el-icon-document" />数据报表
+          </template>
+          <el-menu-item index="qwt-dashboard">
+            <router-link :to="{ name: 'bw-plus-dashboard'}" tag="p">
+              易百搜推广报表
+            </router-link>
+          </el-menu-item>
+        </el-submenu>
+      </el-menu>
+    </main>
+  </div>
+</template>
+
+<script>
+import { version } from '../../../../package.json'
+import BxIcon from 'com/widget/icon'
+
+import {
+  relationAllow,
+  allowSeeDiamondSite,
+  checkRoles,
+  normalizeRoles
+} from 'util/role'
+
+import { getUserSites } from 'api/diamond-site'
+import { baxUserLogin, kaOnlineAndTickets } from 'api/ka'
+
+const MENU_GROUP_MAP = {
+  charge: ['qwt-charge', 'seo-charge'],
+  sst: ['qwt-create-promotion', 'qwt-promotion-list'],
+  bw: ['bw-plan-list'],
+  bwPlus: ['bw-plus-query-price', 'bw-plus-price-records', 'bw-plus-package-list'],
+  qc: ['qc-promote-list'],
+  ssp: ['ad-list', 'material-list', 'order-list', 'user-list', 'ad-calendar'],
+  dashboard: ['qwt-dashboard', 'bw-dashboard', 'qc-dashboard', 'bw-plus-dashboard', 'ad-dashboard']
+}
+
+export default {
+  name: 'sidebar',
+  components: {
+    BxIcon
+  },
+  props: {
+    userInfo: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      version,
+      defaultActive: null,
+      defaultOpeneds: [],
+      isRenderSiteLink: false,
+      isRenderSiteNavTag: false,
+      relationAllow,
+      isDiamondSiteJumpToMainSite: false,
+      isKaSuperman: false
+    }
+  },
+  computed: {
+    allowSeeQueryPrice () {
+      const { roles } = this.userInfo
+      const currentRoles = normalizeRoles(roles)
+      return checkRoles(currentRoles, ['YBS_SALES'])
+    },
+    allowSeeRecord () {
+      const { roles } = this.userInfo
+      const currentRoles = normalizeRoles(roles)
+      return checkRoles(currentRoles, ['YBS_ACCOUNTING', 'YBS_SALES'])
+    },
+    allowSeeManage () {
+      const { isYibaisouUser } = this.userInfo
+      const { roles } = this.userInfo
+      const currentRoles = normalizeRoles(roles)
+      return (isYibaisouUser || checkRoles(currentRoles, ['YBS_SALES']))
+    },
+    allowSeeDianpu () {
+      const { isYibaisouUser } = this.userInfo
+      return isYibaisouUser
+    }
+  },
+  watch: {
+    $route (route) {
+      this.defaultOpeneds = Object
+        .entries(MENU_GROUP_MAP)
+        .reduce((defaultOpeneds, [groupIndex, group]) => {
+          if (group.some(item => item === route.name)) {
+            return defaultOpeneds.concat(groupIndex)
+          }
+          return defaultOpeneds
+        }, [])
+      this.defaultActive = route.name
+    },
+    'userInfo.roles' () {
+      // TIP: 只有普通销售会去
+      if (allowSeeDiamondSite(this.userInfo.roles)) {
+        this.initDiamondSiteNav()
+      }
+    }
+  },
+  async mounted () {
+    await this.initKaNav()
+  },
+  methods: {
+    goKaSuperPage () {
+      location.href = '/ka/vendor/site'
+    },
+    async initKaNav () {
+      this.isKaSuperman = ((await baxUserLogin()).data.roles || []).includes('seo_vendor')
+
+      const { hasSitesAndTickets } = await kaOnlineAndTickets()
+      this.isRenderSiteLink = hasSitesAndTickets
+    },
+    async initDiamondSiteNav () {
+      const hasDiamondSite = !!(await getUserSites())
+      this.isDiamondSiteJumpToMainSite = hasDiamondSite
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+$active-bg: #fff7eb;
+
+.iconfont {
+  margin-right: 13px;
+  font-size: 14px;
+}
+
+.link {
+  display: inline-flex;
+  align-items: center;
+  color: #e1e4ee;
+  cursor: pointer;
+}
+
+.sidebar {
+  position: fixed;
+  top: 50px;
+  left: 0;
+  bottom: 0;
+  z-index: 1000;
+  display: flex;
+  flex-flow: column;
+  background: #fff;
+  min-width: 180px;
+  width: 180px;
+  border-right: 1px solid #e6e6e6;
+  & > main {
+    margin-top: 12px;
+  }
+}
+
+.sidebar {
+  & /deep/ .el-menu-item {
+    height: 48px;
+    line-height: 48px;
+    min-width: unset;
+    padding-right: 0;
+    &:hover {
+      background-color: $active-bg !important;
+    }
+    &.is-active {
+      background-color: $active-bg !important;
+    }
+  }
+  & /deep/ .el-submenu__title {
+    height: 48px;
+    line-height: 48px;
+    &:hover {
+      background-color: $active-bg !important;
+    }
+  }
+  & /deep/ .el-icon-news,
+  & /deep/ .el-icon-document {
+    text-align: left;
+    margin-right: 1px;
+    font-size: 16px;
+  }
+  & .el-icon-question {
+    margin-top: -2px;
+    margin-left: -3px;
+    color: rgb(255, 99, 80);
+    font-size: 16px;
+  }
+
+  ul.el-menu.el-menu--inline > .el-menu-item > * {
+    padding-left: 8px;
+  }
+}
+</style>
---
+++ component/yibaisou-fengming/index.vue
@@ -0,0 +1,289 @@
+
+<template>
+  <div class="container" v-loading.fullscreen="fullscreenLoading">
+    <Header :userInfo="currentUser"/>
+    <div class="main-content">
+      <router-view class="view"
+        :key="$route.fullPath"
+        :userInfo="currentUser"
+        :salesInfo="salesInfo"
+        :allCategories="allCategories"
+        :allAreas="allAreas"
+        :allRoles="allRoles">
+      </router-view>
+    </div>
+    <sidebar :user-info="currentUser"></sidebar>
+    <new-user-intro
+      :mode="newUserIntroMode"
+      :visible="showNewUserIntro"
+      @close="showNewUserIntro = false"
+    />
+    <add-user-lead
+      :user-info="currentUser"
+      :visible="addUserLeadVisible"
+      @close="toggleAddUserLeadVisible"
+    />
+    <huo-dong-btn v-if="inActivityPeriod" />
+    <div class="right-utils">
+    <chat />
+    <back-to-top />
+    <Notification />
+    </div>
+  </div>
+</template>
+
+<script>
+import NewUserIntro from '../common/new-user-intro'
+import Notification from '../common/notification'
+import AddUserLead from '../common/add-user-lead'
+import BackToTop from '../widget/back-to-top'
+import Sidebar from './components/sidebar'
+import Header from './components/header'
+import Chat from '../widget/chat'
+import HuoDongBtn from '../common/huodong-btn'
+
+import gStore from '../store'
+import aStore from '../activity-store'
+
+import track from 'util/track'
+import {
+  normalizeRoles,
+  isNormalUser,
+  isYibaisouSales
+} from 'util/role'
+
+import qs from 'query-string'
+import { Message } from 'element-ui'
+import { redirect } from 'util'
+// import { router } from '../template/bax'
+
+export default {
+  name: 'YibaisouFengming',
+  components: {
+    NewUserIntro,
+    Notification,
+    AddUserLead,
+    // WechatQrcode,
+    // BwShoppingCart,
+    // WechatScan,
+    BackToTop,
+    Sidebar,
+    Header,
+    Chat,
+    HuoDongBtn
+  },
+  fromMobx: {
+    addUserLeadVisible: () => gStore.addUserLeadVisible,
+    currentUser: () => gStore.currentUser,
+    inActivityPeriod: () => aStore.inActivityPeriod,
+    allCategories: () => gStore.allCategories,
+    allAreas: () => gStore.allAreas,
+    allRoles: () => gStore.allRoles
+  },
+  data () {
+    return {
+      showNewUserIntro: false,
+      // isBwRoute: false,
+      newUserIntroMode: '',
+      pending: 0,
+      huoDongIntroVisible: !document.referrer.includes('/a/quanwangtong'),
+      salesInfo: {
+        salesId: '',
+        userId: ''
+      }
+    }
+  },
+  computed: {
+    fullscreenLoading () {
+      return this.pending > 0
+    },
+    topCategories () {
+      return this.allCategories
+        .filter(c => c.level === 1)
+        .map(c => ({
+          label: c.nameCn,
+          id: c.id
+        }))
+    }
+  },
+  methods: {
+    toggleAddUserLeadVisible () {
+      gStore.toggleAddUserLeadVisible()
+    }
+  },
+  async created () {
+    // 记录销售的客户id等信息
+    // 米奇跳转userId需改成user_id
+    // eslint-disable-next-line
+    const { user_id, user_id: userId, sales_id: salesId } = qs.parse(location.search)
+    // eslint-disable-next-line
+    const uid = userId || user_id
+    if (uid && salesId) {
+      this.salesInfo.userId = +uid
+      this.salesInfo.salesId = +salesId
+    }
+  },
+  async mounted () {
+    await Promise.all([
+      gStore.getCurrentUser(),
+      gStore.getCategories(),
+      gStore.getAreas(),
+      gStore.getRoles()
+    ])
+    const { roles, isYibaisouUser } = this.currentUser
+    if (isNormalUser(roles)) {
+      if (!isYibaisouUser) {
+        Message.error('您没有权限访问，请更换帐号登陆')
+        return redirect('signin', `return=${encodeURIComponent(location.pathname + location.search)}`)
+      }
+    } else {
+      if (!isYibaisouSales(roles)) {
+        Message.error('您没有权限访问，请更换帐号登陆')
+        return redirect('signin', `return=${encodeURIComponent(location.pathname + location.search)}`)
+      }
+    }
+    setTimeout(() => {
+      const { currentUser } = this
+      // move to user intro component
+      const roles = normalizeRoles(currentUser.roles)
+      if (this.currentUser.isNewUser === 1 &&
+        roles.includes('BAIXING_USER')) {
+        this.showNewUserIntro = true
+      } else if (Date.now() < 1529596800000) {
+        const times = localStorage.getItem('bx-qt-sm-al-t') | 0
+        if (times === 0) {
+          // 初次
+          this.newUserIntroMode = 'shenma'
+          this.showNewUserIntro = true
+          localStorage.setItem('bx-qt-sm-al-lt', Date.now())
+          localStorage.setItem('bx-qt-sm-al-t', 1)
+        } else if (times < 3) {
+          const lastTime = parseInt(localStorage.getItem('bx-qt-sm-al-lt'))
+          if ((Date.now() - lastTime) > 24 * 60 * 60 * 1000) {
+            this.newUserIntroMode = 'shenma'
+            this.showNewUserIntro = true
+            localStorage.setItem('bx-qt-sm-al-lt', Date.now())
+            localStorage.setItem('bx-qt-sm-al-t', times + 1)
+          }
+        }
+      }
+
+      track({
+        action: 'bax: enter page',
+        baixingId: currentUser.baixingId,
+        baxId: currentUser.id,
+        src: this.$route.query.src || ''
+      })
+    }, 1200)
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .notice {
+    display: flex;
+    position: fixed;
+    left: 190px;
+    top: 0;
+    width: 35%;
+    color: #f44336;
+    background-color: white;
+    font-size: .8em;
+    z-index: 1000;
+    box-shadow: 1px 2px 1px #ddd;
+
+    & > .close {
+      color: gray;
+      font-size: 1.5em;
+      cursor: pointer;
+    }
+  }
+  .main-content {
+    width: 100%;
+    min-width: 1366px;
+    padding-top: 50px;
+    padding-left: 180px;
+    background-color: #eeeff0;
+    overflow-x: hidden;
+    scroll-behavior: smooth;
+    position: absolute;
+    height: 100%;
+  }
+  .view {
+    min-height: calc(100vh - 50px);
+    box-sizing: border-box;
+    padding: 12px 12px 32px;
+  }
+</style>
+
+<style lang="scss">
+* {
+  font-family: PingFangSC-Regular, 'Open Sans', 'Microsoft YaHei', 微软雅黑, STHeiti, 'WenQuanYi Micro Hei', SimSun, sans-serif;
+  -webkit-tap-highlight-color: transparent;
+  vertical-align: baseline;
+  box-sizing: border-box;
+  font-size: 100%;
+  outline: 0;
+  padding: 0;
+  margin: 0;
+  border: 0;
+}
+
+html {
+  font-family: 'Hiragino Sans GB', helvetica, tahoma, arial, sans-serif;
+}
+
+html, body {
+  height: 100%;
+}
+
+a {
+  background-color: transparent;
+  text-decoration: none;
+}
+
+i {
+  font-style: normal;
+}
+
+ul, li {
+  list-style: none;
+}
+
+a, span, aside, label, strong {
+  display: inline-block;
+}
+
+textarea {
+  resize: none;
+}
+
+dialog {
+  margin: auto;
+}
+
+body > .container {
+  display: block;
+  min-width: 1320px;
+}
+
+.el-tooltip__popper {
+  max-width: 340px;
+}
+
+.el-input__prefix, .el-input__suffix {
+  & > span {
+    display: unset;
+  }
+}
+
+.el-loading-mask {
+  height: 100%;
+}
+.right-utils{
+    position: fixed;
+    right: 20px;
+    bottom: 160px;
+    z-index: 3;
+}
+</style>
