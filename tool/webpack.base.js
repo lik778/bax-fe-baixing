44e6bd4b17e74397e652c26c692259e1b4da0845
likuan <likuan@baixing.com>
2022-08-03 16:25:32 +0800

    fix: test5

--- tool/webpack.base.js
+++ tool/webpack.base.js
@@ -23,8 +23,8 @@ module.exports = {
     bax: './template/bax',
     huodong: './template/huodong',
     authorizationPage: './template/authorization-page',
-    yibaisou: './template/yibaisou',
-    fengming: './template/fengming'
+    yibaisou: './template/yibaisou'
+    // fengming: './template/fengming'
   },
   output: {
     path: distPath,
