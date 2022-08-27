0d10fc714efd8332b7359f1f6f07189bc9f48d7f
李款 <likuan@baixing.com>
2022-08-27 15:12:40 +0800

    update

--- tool/move-file.js
+++ tool/move-file.js
@@ -4,11 +4,14 @@ const { join } = require('path')
 const fs = require('fs')
 
 const assets = fs.readdirSync(join(__dirname, '../dist'))
-
 for (const asset of assets) {
   const [, _, ext] = asset.split('.')
   if (ext === 'html' || _ === 'html') {
-    fs.renameSync(
+    // fs.renameSync(
+    //   join(__dirname, '../dist/', asset),
+    //   join(__dirname, '../view/', asset)
+    // )
+    fs.copyFileSync(
       join(__dirname, '../dist/', asset),
       join(__dirname, '../view/', asset)
     )
